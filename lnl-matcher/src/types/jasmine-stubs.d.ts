5d3cec524a4fc4329ce3cef07dceec5c095a3d40
NathanWallerViz <137112435+NathanWallerViz@users.noreply.github.com>
2025-05-29 11:30:22 -0500

    Revert "Fix TypeScript compilation"

--- lnl-matcher/src/types/jasmine-stubs.d.ts
+++
@@ -1,4 +0,0 @@
-declare function describe(desc: string, spec: () => void): void;
-declare function it(desc: string, spec: () => void): void;
-declare function expect(actual: any): any;
-declare function beforeEach(fn: () => void): void;
