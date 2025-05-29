3524f8c76a79d84659118fa6c26634936f683df8
NathanWallerViz <137112435+NathanWallerViz@users.noreply.github.com>
2025-05-29 11:16:56 -0500

    Add match requests table with local storage

--- lnl-matcher/src/app/home/home.component.ts
+++ lnl-matcher/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 
@@ -7,8 +7,17 @@ import { UserService } from '../user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+interface MatchRequest {
+  name: string;
+  department: string;
+  interests: string;
+  requestDate: string;
+  scheduledDate?: string;
+}
+
+export class HomeComponent implements OnInit {
   activeTab = 'Match';
+  requestsTab = 'Incoming';
   departments = [
     'Viztech',
     'Reporting',
@@ -31,6 +40,68 @@ export class HomeComponent {
   interest = '';
   interests: string[] = [];
   showSuccess = false;
+
+  matchRequests: MatchRequest[] = [];
+
+  ngOnInit() {
+    const data = localStorage.getItem('matchRequests');
+    if (data) {
+      this.matchRequests = JSON.parse(data);
+    } else {
+      this.generateSampleRows();
+    }
+  }
+
+  private saveRequests() {
+    localStorage.setItem('matchRequests', JSON.stringify(this.matchRequests));
+  }
+
+  private generateSampleRows() {
+    const samples: MatchRequest[] = [];
+    for (let i = 1; i <= 5; i++) {
+      samples.push({
+        name: `User ${i}`,
+        department: this.departments[i % this.departments.length],
+        interests: 'Interest ' + i,
+        requestDate: new Date().toISOString().slice(0, 10),
+      });
+    }
+    this.matchRequests = samples;
+    this.saveRequests();
+  }
+
+  setRequestsTab(tab: string) {
+    this.requestsTab = tab;
+  }
+
+  filteredRequests() {
+    return this.matchRequests.filter(r =>
+      this.requestsTab === 'Incoming' ? !r.scheduledDate : !!r.scheduledDate
+    );
+  }
+
+  openDatePicker(input: HTMLInputElement) {
+    if ((input as any).showPicker) {
+      (input as any).showPicker();
+    } else {
+      input.click();
+    }
+  }
+
+  scheduleRequest(req: MatchRequest, date: string) {
+    if (date) {
+      req.scheduledDate = date;
+      this.saveRequests();
+    }
+  }
+
+  denyRequest(req: MatchRequest) {
+    const index = this.matchRequests.indexOf(req);
+    if (index >= 0) {
+      this.matchRequests.splice(index, 1);
+      this.saveRequests();
+    }
+  }
 
   constructor(public userService: UserService, private router: Router) {}
 
